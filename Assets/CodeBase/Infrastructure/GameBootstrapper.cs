53b0e3cef09117b3c49762058791843780592fc9
ZeppSoft <zeppsoft@gmail.com>
2025-08-10 21:45:35 +0300

    Init2

---
+++ Assets/CodeBase/Infrastructure/GameBootstrapper.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+namespace CodeBase.Infrastructure
+{
+    /// <summary>
+    /// The GameBootstrapper class is responsible for initializing the game.
+    /// It is executed once before the first Update call after the MonoBehaviour is created.
+    /// </summary>
+    public class GameBootstrapper : MonoBehaviour
+    {
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        private void Awake()
+        {
+
+        }
+    }
+}
